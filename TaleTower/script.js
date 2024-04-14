4730f2fd1d59ec02c8c3639babc149aecccabfc6
Fatma Atta <87873165+FatmaAtta@users.noreply.github.com>
2024-04-14 22:50:36 +0200

    latest

--- TaleTower/script.js
+++ TaleTower/script.js
@@ -1,61 +1,110 @@
-// let count=0;
-// let library=[];
+// function addBookUserPage(){
+//     const title=localStorage.getItem('title');
+//     const author=localStorage.getItem('author');
+//     const category=localStorage.getItem('category');
+//     const imageURL=localStorage.getItem('imageURL');
+//     const description=localStorage.getItem('description');
+    
+//     let book=document.createElement('div');
+//     book.classList.add('book');
+//     book.innerHTML=
+//     `<a href="Deliverance_book.html" target="_blank">
+//     <img src="${imageURL}">
+//     </a>`;
+//     document.getElementById('available-books').appendChild(book);
+//     alert('Added Book Successfully!');
+// }
 
-let addBookForm = document.getElementById('addBookForm');
+let library=[];
+let addBookForm = document.getElementById('addBook');
+
 addBookForm.addEventListener('submit', function(event){
     event.preventDefault();
-    count=count+1;
-    let title=document.getElementById('title').value;
-    let author=document.getElementById('author').value;
-    let category=document.getElementById('category').value;
-    let imageURL=document.getElementById('imageURL').value;
-    let description=document.getElementById('description').value;
-    addBook(title,author,category,imageURL,description);
-    console.log('Book added:', book);
+    const title=document.getElementById('title').value;
+    const author=document.getElementById('author').value;
+    const category=document.getElementById('category').value;
+    const imageURL=document.getElementById('imageURL').value;
+    const description=document.getElementById('description').value;
+    let book = {
+        title: title,
+        author: author,
+        category: category,
+        image: imageURL,
+        description: description
+    };
+    let libraryString=localStorage.getItem('library');
+    if(!libraryString){
+        library.push(book);
+        saveLocal(library);
+    }
+    else{
+        let libraryStringParse=JSON.parse(localStorage.getItem('library'));
+        libraryStringParse.push(book);
+        saveLocal(libraryStringParse);
+    }
 });
 
-function addBook(title, author, category, imageURL, description){
-    // let book={
-    //     id:count,
-    //     title:title,
-    //     author:author,
-    //     category:category,
-    //     imageURL:imageURL,
-    //     description:description,
-    //     status:'A'
-    // };
-    // library.push(book);
-    // console.log(book);
-    // console.log(library);
-    alert('Added Book Successfully!');
+function saveLocal(library){
+    let libraryStringify=JSON.stringify(library);
+    localStorage.setItem('library',libraryStringify);
 }
 
-function borrowBook(book){
-    book.status='B';
-}
+window.addEventListener('storage',function(event){
+    if(event.key == 'library'){
+        const updatedLibrary = JSON.parse(event.newValue);
+        const newItem=updatedLibrary[updatedLibrary.length-1];
+        const book=document.createElement('div');
+        book.classList.add('book');
+        book.innerHTML=`
+        <a href="Deliverance_book.html" target="_blank">
+           <img src="${newItem.imageURL}">
+        </a>
+        `;
+        document.getElementById('available-books').appendChild(book);
+    }
 
-function returnBook(title, author, category,imageURL, description){
-    let book={
-        title:title,
-        author:author,
-        category:category,
-        imageURL:imageURL,
-        description:description
-    };
-    borrowed.push(book);
-}
+});
+
+// function addBookAdminPage(title, author, category, imageURL, description){
+    
+// }
+
+// function borrowBook(book){
+//     book.status='B';
+// }
+
+// function returnBook(title, author, category,imageURL, description){
+//     let book={
+//         title:title,
+//         author:author,
+//         category:category,
+//         imageURL:imageURL,
+//         description:description
+//     };
+//     borrowed.push(book);
+// }
 
 
 //search 
 
-function findCategory(category){
-    return library.filter(book=>book.category==category);
-}
-function findAuthor(author){
-    return library.filter(book=>book.author==author);
-}
+// function findCategory(category){
+//     return library.filter(book=>book.category==category);
+// }
+// function findAuthor(author){
+//     return library.filter(book=>book.author==author);
+// }
 
-function saveChanges(){
+// function saveChanges(){
 
+// }
+//test
+function addItem(){
+        const book=document.createElement('div');
+        book.classList.add('book');
+        book.innerHTML=`
+        <a href="Deliverance_book.html" target="_blank">
+           <img src="deliverance.jpeg">
+        </a>
+        `;
+        document.getElementById('available-books').appendChild(book);
 }
-
