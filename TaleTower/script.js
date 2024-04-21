43a14dd54617a80d52fb379ad7233106c1688494
a-ghandour <123655005+a-ghandour@users.noreply.github.com>
2024-04-21 13:35:34 +0200

    second main page js functions

--- TaleTower/script.js
+++ TaleTower/script.js
@@ -108,3 +108,33 @@ function addItem(){
         `;
         document.getElementById('available-books').appendChild(book);
 }
+
+
+function loadButtonText() {
+    var buttons = document.getElementsByClassName("sec-main borrowbtn");
+    for (var i = 0; i < buttons.length; i++) {
+        var buttonText = sessionStorage.getItem("buttonText" + i);
+        if (buttonText) {
+            buttons[i].innerText = buttonText;
+        }
+    }
+}
+function saveButtonText() {
+    var buttons = document.getElementsByClassName("sec-main borrowbtn");
+    for (var i = 0; i < buttons.length; i++) {
+        sessionStorage.setItem("buttonText" + i, buttons[i].innerText);
+    }
+}
+function changeButton(){
+var buttons = document.getElementsByClassName("sec-main borrowbtn");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        this.innerText = "Borrowed";
+        saveButtonText();
+    });
+}
+}
+
+window.addEventListener("load", function() {
+    loadButtonText();
+});
